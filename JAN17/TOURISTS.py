c4d4a6f916351eb55eed47d0299345a21f34b693
Ankur Jain <ankurjain8448@gmail.com>
2017-01-24 12:54:25 +0530

    TOURISTS.py

--- JAN17/TOURISTS.py
+++ JAN17/TOURISTS.py
@@ -13,6 +13,7 @@ class Graph(object):
 
 	def eulerPath(self):
 		g = self.graph
+		# getting nodes whose degree is odd
 		odd = [k for k, v  in g.iteritems() if len(v)%2 == 1]
 		if len(odd) == 0 :
 			odd = [g.keys()[0]]
@@ -50,6 +51,7 @@ if ans is None:
 else:
 	if len(ans) == (e+1) and ans[0] == ans[-1]:
 		print "YES"
+		# create temp_dict for fast mapping of input edges
 		temp_dict = {}
 		for i in xrange(len(ans)-1,0,-1):
 			if ans[i] in temp_dict:
@@ -58,8 +60,10 @@ else:
 				temp_dict[ans[i]] = {ans[i-1]:True}
 		for i in xrange(e):
 			if u[i] in temp_dict and v[i] in temp_dict[u[i]]:
+				# if node is not reversed
 				print u[i] , v[i]
 			else:
+				# if node is reversed or if the pair is not present in temp_dict
 				print v[i], u[i]
 	else:
 		print "NO"
